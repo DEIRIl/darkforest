30521832e7511a8d705e24a375d8cc01893cbdcb
DCRIl <kelehsaev.2007@gmail.com>
2024-01-03 14:15:07 +0300

    true commit

--- classPlayer.py
+++ classPlayer.py
@@ -4,12 +4,13 @@ from libraryImages import load_image
 
 
 class Player(pygame.sprite.Sprite):
-    image1 = load_image("player.png")
+    image1 = load_image("player2.png", -1)
     image2 = pygame.transform.flip(image1, True, False)
-    def __init__(self, x, y, radius, speed, all_sprites, size):
+
+    def __init__(self, x, y, radius, speed, all_sprites, size, screen):
         super().__init__(all_sprites)
         self.radius = radius
-        self.image = pygame.transform.scale(Player.image1, (0.75 * radius, 1.75 * radius))
+        self.image = pygame.transform.scale(Player.image1, (radius, 1.5 * radius))
         self.rect = self.image.get_rect().move(x, y)
         self.mask = pygame.mask.from_surface(self.image)
         self.x1motoin = "n"
@@ -20,33 +21,67 @@ class Player(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.screen_size = size
-        self.jump = False
-        self.xx = 0
+        self.jump_max = 20
+        self.jump_count = 0
+        self.max_hp = 100
+        self.hp = 100
+        self.sc = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.screen = screen
 
-    def update(self, objects, floor):
+    def show_hp(self):
+        pygame.draw.rect(self.sc, "grey", (28, self.screen_size[1] - 77, 304, 34))
+        pygame.draw.rect(self.sc, "green", (30, self.screen_size[1] - 75, 3 * self.hp, 30))
+        pygame.draw.rect(self.sc, "red", (30 + 3 * self.hp, self.screen_size[1] - 75, 3 * (self.max_hp - self.hp), 30))
+        self.screen.blit(self.sc, (0, 0))
+
+    def death(self):
+        pass
+
+
+    def update(self, jump, fall, objects, floor):
         if self.x1motoin == "y" and self.x2motoin == "y":
             self.vx = 0
         elif self.x1motoin == "y":
             self.vx = self.v
-            self.image = pygame.transform.scale(Player.image1, (0.75 * self.radius, 1.75 * self.radius))
+            self.image = pygame.transform.scale(Player.image1, (self.radius, 1.5 * self.radius))
         elif self.x2motoin == "y":
             self.vx = -self.v
-            self.image = pygame.transform.scale(Player.image2, (0.75 * self.radius, 1.75 * self.radius))
+            self.image = pygame.transform.scale(Player.image2, (self.radius, 1.5 * self.radius))
         else:
             self.vx = 0
-        if self.jump:
-            self.xx = 30
+        if jump:
+            self.vy = -self.jump_count
+            if self.jump_count > 0 and not pygame.sprite.spritecollideany(self,
+                                                                          floor) and not pygame.sprite.spritecollideany(
+                    self, objects):
+                self.jump_count -= 1
+            elif not pygame.sprite.spritecollideany(self, floor) and not pygame.sprite.spritecollideany(self, objects):
+                jump = False
+            elif pygame.sprite.spritecollideany(self, floor) and pygame.sprite.spritecollideany(self, objects):
+                jump = False
+                self.jump_count = 0
+                self.vy = 0
+        elif fall and self.jump_count <= 0:
+            if not pygame.sprite.spritecollideany(self, floor) and pygame.sprite.spritecollideany(self, objects):
+                self.jump_count -= 2
+                self.vy = -self.jump_count
+            elif not pygame.sprite.spritecollideany(self, floor) and not pygame.sprite.spritecollideany(self, objects):
+                fall = False
+                self.vy = -self.jump_count
+                self.jump_count = self.vy
+            elif pygame.sprite.spritecollideany(self, floor):
+                fall = False
+                self.vy = 0
+        else:
             self.jump = False
-        if self.xx == 0:
-            self.vy = self.v
-        elif self.xx != 0:
-            self.vy = -self.v
-            self.xx -= 1
-        if pygame.sprite.spritecollideany(self, floor):
-            self.vy -= self.v
-        # if self.xx == 0 and pygame.sprite.spritecollideany(self, objects):
-        #     self.vy -= self.v
-        if pygame.sprite.spritecollideany(self, objects):
-            self.xx = 0
-            # self.vy -= self.v
+            fall = False
+            self.vy = self.jump_count if self.jump_count != 0 else self.vy
+            self.jump_count += 1
+            if pygame.sprite.spritecollideany(self, floor) or pygame.sprite.spritecollideany(self, objects):
+                self.vy = 0
+                self.jump_count = 0
         self.rect = self.rect.move(self.vx, self.vy)
+        # print(jump, fall, self.jump_count)
+        if self.hp == 0:
+            self.death()
+        return (jump, fall)
